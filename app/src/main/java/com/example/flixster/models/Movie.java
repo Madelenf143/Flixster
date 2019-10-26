bbefc73db3ca5edd613b064ea8a14a8d8bacdeda
Madelen <madfloven143@gmail.com>
2019-10-25 22:40:14 -0700

    FLixster Version #2

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,24 +3,38 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+    int movieId;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+
+    public Movie() {    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString( "poster_path");
+        backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString( "title");
         overview = jsonObject.getString( "overview");
+        rating = jsonObject.getDouble( "vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public String getPosterPath() {
         return String.format( "https://image.tmdb.org/t/p/w342/%s", posterPath);
 
+    }
+    public String getBackdropPath (){
+        return String.format( "https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 
     public String getTitle() {
@@ -38,4 +52,13 @@ public class Movie {
         }
         return movies;
     }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
 }
+
